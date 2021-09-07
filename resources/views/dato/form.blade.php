d9170ea6f9ef21636c1d641f2a00cb8ab532e4d4
Kevin Fabricio Morocho Almeida <65823017+KevinMorocho@users.noreply.github.com>
2021-09-07 12:11:15 -0500

    Rutas de Laravel

--- resources/views/dato/form.blade.php
+++ resources/views/dato/form.blade.php
@@ -105,7 +105,7 @@
 <br>
 <div class="row">
     <div class="col-md-6">
-        <a href="{{ route('datos.index') }}" class="btn btn-danger btn-block"><i class="far fa-arrow-alt-circle-left"> </i> Regresar</a>
+        <a href="{{route('datos.index')}}" class="btn btn-danger btn-block"><i class="far fa-arrow-alt-circle-left"> </i> Regresar</a>
     </div>
     <div class="col-md-6">
         <button class="btn btn-primary btn-block"><i class="far fa-save"> </i> Guardar</button>
