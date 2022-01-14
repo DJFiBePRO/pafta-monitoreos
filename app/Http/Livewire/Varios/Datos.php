11ee9a118519c772f058e2576cacd3b5740833d4
Carlos Fiallos <carlosfiallos@hotmail.es>
2022-01-14 15:15:26 -0500

    en main

--- app/Http/Livewire/Varios/Datos.php
+++ app/Http/Livewire/Varios/Datos.php
@@ -17,7 +17,8 @@ class Datos extends Component
 
     protected $paginationTheme = 'bootstrap';
 
-    private $pagination = 3;
+    private $pagination = 5;
+    public $cant;
 
     public function paginationView()
     {
@@ -29,15 +30,6 @@ class Datos extends Component
         /*$data=DB::table('users')
         ->join()*/
 
-        $tecnicos = User::paginate(2);
-
-
-        return view('livewire.varios.datos', compact('tecnicos'))
-        ->extends('adminlte::page');
-    }
-    public function vista($id)
-    {
-        $si='si';
         $datos = DB::table('monitoreos')
         ->join('estudios', 'estudios.id', '=', 'monitoreos.idEstudio')
         ->join('datos', 'monitoreos.id', '=', 'datos.idMonitoreo')
@@ -51,9 +43,32 @@ class Datos extends Component
         ->select('plantas.codigo as planta','datos.fruto as fruto','datos.incidencia as incidencia','datos.severidad as severidad','fincas.nombreFinca as finca','monitoreos.fechaEjecucion as fecha','fincas.densidad as densidad','cantons.nombre as canton','parroquias.nombre as parroquia','variedads.descripcion as variedad', 'monitoreos.estado')
         //->where('monitoreos.idTecnico',$id)
         //->where('monitoreos.estado',$si)
-        ->get();//Ya funciona xD
-        return view('varios.datos',compact('datos'));
+        ->paginate($this->pagination);//Ya funciona xD
+
+
+        return view('livewire.varios.datos', compact('datos'))
+        ->extends('adminlte::page');
     }
+    // public function vista($id)
+    // {
+    //     $si='si';
+    //     $datos = DB::table('monitoreos')
+    //     ->join('estudios', 'estudios.id', '=', 'monitoreos.idEstudio')
+    //     ->join('datos', 'monitoreos.id', '=', 'datos.idMonitoreo')
+    //     ->join('finca_variedad', 'estudios.idFv', '=', 'finca_variedad.id')
+    //     ->join('fincas', 'finca_variedad.finca_id', '=', "fincas.id")
+    //     ->join('variedads','finca_variedad.variedad_id','=','variedads.id')
+    //     ->join('plantas','datos.idPlanta', '=', 'plantas.id')
+    //     ->join('zonas','fincas.idZona', '=','zonas.id')
+    //     ->join('parroquias','zonas.idParroquia','=','parroquias.id')
+    //     ->join('cantons','parroquias.idCanton','=','cantons.id')
+    //     ->select('plantas.codigo as planta','datos.fruto as fruto','datos.incidencia as incidencia','datos.severidad as severidad','fincas.nombreFinca as finca','monitoreos.fechaEjecucion as fecha','fincas.densidad as densidad','cantons.nombre as canton','parroquias.nombre as parroquia','variedads.descripcion as variedad', 'monitoreos.estado')
+    //     //->where('monitoreos.idTecnico',$id)
+    //     //->where('monitoreos.estado',$si)
+        
+    //     ->get();//Ya funciona xD
+    //     return view('livewire.varios.datos',compact('datos'))->extends('adminlte::page');;
+    // }
 
     public function exportExcel()
     {
