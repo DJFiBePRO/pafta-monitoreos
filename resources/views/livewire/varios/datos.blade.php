253d0646d4f104672153ec6283057ff0f5f730e1
Carlos Fiallos <carlosfiallos@hotmail.es>
2022-01-14 15:13:18 -0500

    Un solo archivo de datatable añadido
    Co-authored-by: Kevin Fabricio Morocho Almeida <KevinMorocho@users.noreply.github.com>

--- resources/views/livewire/varios/datos.blade.php
+++ resources/views/livewire/varios/datos.blade.php
@@ -1,14 +1,23 @@
 <div class="card">
-    @section('title', 'Ver Datos')
+    @section('title', 'Alertas Tempranas-Datos')
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png"/>
     <div class="justify-content-center">
         <div class="card-header align-items-center">
             <div class="row align-items-center">
                 <div class="col-md-10">
-                    <h1>Técnico</h1>
+                    <h1>Datos Totales</h1> <br>
+                    <span>Mostrar</span>
+                    <select class="mx-2 from-control">
+                        <option value="10">10</option>
+                        <option value="25">25</option>
+                        <option value="50">50</option>
+                        <option value="100">100</option>
+                    </select>
                 </div>
                 <div class="container col-md-2">
                     <div class="text-center justify-content-center">
-                        {{-- <a href="tecnicos/create" class="btn btn-success">Nuevo Registro</a> --}}
+                        <a href="{{ url('descargas/descarga-excel') }}" class="btn btn-success"><i
+                                class="fas fa-file-excel"></i> EXCEL </a>
                     </div>
                 </div>
             </div>
@@ -23,31 +32,48 @@
                 <thead>
                     <tr>
                         <th>#</th>
-                        <th>NOMBRE TÉCNICO</th>
-                        <th>INSTITUCIÓN</th>
-                        <th>TELÉFONO</th>
-                        <th>EMAIL</th>
-                        <th>ACTIVO</th>
-                        <th>ACCIÓN</th>
+                        <th>PLANTA</th>
+                        <th>FRUTO</th>
+                        <th>INCIDENCIA</th>
+                        <th>SEVERIDAD</th>
+                        <th>FINCA</th>
+                        <th>FECHA</th>
+                        <th>CANTON</th>
+                        <th>PARROQUIA</th>
+                        <th>DENSIDAD</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach ($tecnicos as $tecnico)
+                    @foreach ($datos as $dato)
+                    {{-- @if (auth()->user()->fullacces == 'yes') --}}
                     <tr>
                         <td>{{ $count++ }}</td>
-                        <td>{{ $tecnico->name }}</td>
-                        <td>{{ $tecnico->institucion }}</td>
-                        <td>{{ $tecnico->telefono }}</td>
-                        <td>{{ $tecnico->email }}</td>
-                        <td>{{ $tecnico->activo }}</td>
-                        <td><a href="/patfa-monitoreos/public/descarga/{{ $tecnico->id }}" class="btn btn-primary"
-                               >Ir <i class="far fa-arrow-alt-circle-right">
-                                </i> </a></td>
+                        <td>{{ $dato->planta }}</td>
+                        <td>{{ $dato->fruto }}</td>
+                        <td>{{ $dato->incidencia }}</td>
+                        <td>{{ $dato->severidad }}</td>
+                        <td>{{ $dato->finca }}</td>
+                        <td>{{ $dato->fecha}}</td>
+                        <td>{{ $dato->canton }}</td>
+                        <td>{{ $dato->parroquia }}</td>
+                        <td>{{ $dato->densidad }}</td>
                     </tr>
+                    {{-- @endif --}}
                     @endforeach
                 </tbody>
             </table>
-            {{ $tecnicos->links() }}
+            {{$datos->links()}}
         </div>
     </div>
+    {{-- @section('js')
+    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"
+        crossorigin="anonymous"></script>
+    <script type="text/javascript" charset="utf8"
+        src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+    <script type="text/javascript" charset="utf8"
+        src="https://cdn.datatables.net/responsive/2.2.7/js/dataTables.responsive.min.js" crossorigin="anonymous">
+    </script>
+    <script src="{{ asset('js/datatable-general.js')}}"></script>
+
+    @stop --}}
 </div>
